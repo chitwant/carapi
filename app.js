0798eb8c843b94e8ca1e36536694e85863f46341
chitwant <guri.preet@gmail.com>
2018-09-03 17:18:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ mongoose.connect(config.database)
 
 var app = express();
 app.use(cors())
-app.options('*',cors());
+// app.options('*',cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
