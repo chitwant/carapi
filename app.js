3772a7daa71eb2f5051a4c52fc2f542621ef221a
chitwant <guri.preet@gmail.com>
2018-09-03 17:06:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -42,25 +42,6 @@ app.use('/users', usersRouter);
 app.use('/cars', carsRouter);
 app.use('/addcar', addCar);
 
-// Add headers
-app.use(function (req, res, next) {
-
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -81,5 +62,23 @@ app.use(function(err, req, res, next) {
 });
 
 
+// Add headers
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 module.exports = app;
